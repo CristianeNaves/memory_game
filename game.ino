6945470867ba59561b5400aec0d167959a2af660
CristianeNaves <cristianenavescardoso09@gmail.com>
2016-11-07 16:05:25 -0200

    adicionando funcionalidade digitar

--- game.ino
+++ game.ino
@@ -2,54 +2,92 @@
 
 
 int LED_RED = 12;
+int LED_RED_USER = 3;
 int LED_GREEN = 11;
+int LED_GREEN_USER = 7;
 int BUTTON = 10;
-int count = 1;
+int BUTTON_RED = 2;
+int BUTTON_GREEN = 4;
+int contador = 1;
 int LED;
+bool sortLed = false;
+bool digitarLed = false;
 QueueList <int> listaRandom;
-//QueueList <int> listaDigitada;
+QueueList <int> listaDigitada;
 
 void setup() {
  Serial.begin(9600);
  pinMode(LED_RED, OUTPUT);
+ pinMode(LED_RED_USER, OUTPUT);
+ pinMode(LED_GREEN_USER, OUTPUT);
  pinMode(LED_GREEN, OUTPUT);
  pinMode(BUTTON, INPUT);
+ pinMode (BUTTON_RED, INPUT);
+ pinMode (BUTTON_GREEN, INPUT);
  randomSeed(analogRead(0));
 }
 
-void removerElementos(){
+void removerElementosRandom(){
   while(!listaRandom.isEmpty ()){
-    listaRandom.pop();
+    Serial.print(listaRandom.pop());
+    //listaRandom.pop();
     }
 }
-
+void removerElementosDigitada(){
+  while(!listaDigitada.isEmpty ()){
+    Serial.print(listaDigitada.pop());
+  }
+}
 
 void loop() {
+  while(!sortLed){ 
   if(digitalRead(BUTTON) == HIGH){
-    for(int i = 0; i < count; i++){
+    Serial.print(contador);
+    for(int i = 0; i < contador; i++){
       LED = random(2);
       if(LED == 0){
-        //listaRandom.push(LED_GREEN);
+        listaRandom.push(LED_GREEN);
         digitalWrite(LED_GREEN, HIGH);
         delay(500);
         digitalWrite(LED_GREEN, LOW);
         delay(500);
       }
       else{
-        //listaRandom.push(LED_RED);
-        //Serial.print(listaRandom.peek());
+        listaRandom.push(LED_RED);
         digitalWrite(LED_RED, HIGH);
         delay(500);
         digitalWrite(LED_RED, LOW);
         delay(500); 
       }
     }
-    //fazer só até 10 depois voltar
-    count++;
-    //removerElementos();
+    sortLed = true;
   }
-  else{
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_RED, LOW);
   }
+    for(int i = 0; i < contador; i++){
+      while(!digitarLed){
+        Serial.print("Digitar Led");
+        if(digitalRead(BUTTON_RED) == HIGH){
+          listaDigitada.push(LED_RED);
+          digitalWrite(LED_RED_USER, HIGH);
+          delay(500);
+          digitalWrite(LED_RED_USER, LOW);
+          digitarLed = true;
+        }
+        if(digitalRead(BUTTON_GREEN) == HIGH){
+          listaDigitada.push(LED_GREEN);
+          digitalWrite(LED_GREEN_USER, HIGH);
+          delay(500);
+          digitalWrite(LED_GREEN_USER, LOW);
+          digitarLed = true;
+        }
+      }
+      sortLed = false;
+      digitarLed = false;
+    }
+    contador++;  
+    removerElementosRandom();
+    removerElementosDigitada();
+ 
+  
+  
 } 
